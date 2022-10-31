103217af95f9bd6643df62f9f36ea2d3e398a5bd
Aliaksei_Ashukha <aliaksei_ashukha@epam.com>
2022-10-31 17:30:13 +0300

    Add shell-installation

---
+++ shell-installation/download_services.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Versions of the services
+BACKEND_VERSION="3.2.4"
+
+# Downloading
+#curl -L https://github.com/healenium/healenium-backend/releases/download/${BACKEND_VERSION}/healenium-backend.jar > hlm-backend.jar
+
+git clone https://github.com/healenium/healenium-selector-imitator.git
---
+++ shell-installation/start_healenium.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Requirements
+HLM_POSTGRES_DB=healenium
+HLM_POSTGRES_SCHEMA=healenium
+HLM_POSTGRES_USER=healenium_user
+HLM_POSTGRES_PASSWORD=YDk2nmNs4s9aCP6K
+HLM_SERVER_PORT=7878
+
+# Deploy the services
+SPRING_POSTGRES_DB=$HLM_POSTGRES_DB SPRING_POSTGRES_SCHEMA=$HLM_POSTGRES_SCHEMA SPRING_POSTGRES_USER=$HLM_POSTGRES_USER SPRING_POSTGRES_PASSWORD=$HLM_POSTGRES_PASSWORD SPRING_SERVER_PORT=$HLM_SERVER_PORT java -jar hlm-backend.jar 2>&1 &
+
+pip install --upgrade pip
+
+pip install -r healenium-selector-imitator/requirements.txt
+
+python3 healenium-selector-imitator/app.py
