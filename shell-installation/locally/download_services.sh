61a1d2b0276be3344716945ce320c7bf1877f271
Aliaksei_Ashukha <aliaksei_ashukha@epam.com>
2022-11-06 14:36:54 +0300

    Update to hlm-backend 3.2.5

--- shell-installation/locally/download_services.sh
+++ shell-installation/locally/download_services.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Versions of the services
-HLM_BACKEND_VERSION="3.2.4"
+HLM_BACKEND_VERSION="3.2.5"
 HLM_PROXY_VERSION="1.2.0"
 
 # Downloading
-curl -L https://github.com/healenium/healenium-backend/releases/download/${HLM_BACKEND_VERSION}/healenium-backend.jar > hlm-backend.jar
+curl -L https://github.com/healenium/healenium-backend/releases/download/${HLM_BACKEND_VERSION}/healenium-backend-${HLM_BACKEND_VERSION}.jar > hlm-backend.jar
 
 git clone https://github.com/healenium/healenium-selector-imitator.git
