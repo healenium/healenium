44677ae21ea116450f30277ac99e63d9cd64960e
Alex-Reif <69298932+Alex-Reif@users.noreply.github.com>
2023-02-27 14:27:09 +0300

    Update download_services.sh

--- shell-installation/locally/download_services.sh
+++ shell-installation/locally/download_services.sh
@@ -2,7 +2,7 @@
 
 # Versions of the services
 HLM_BACKEND_VERSION="3.2.5"
-HLM_PROXY_VERSION="1.2.0"
+HLM_PROXY_VERSION="1.2.1"
 
 # Downloading
 curl -L https://github.com/healenium/healenium-backend/releases/download/${HLM_BACKEND_VERSION}/healenium-backend-${HLM_BACKEND_VERSION}.jar > hlm-backend.jar
