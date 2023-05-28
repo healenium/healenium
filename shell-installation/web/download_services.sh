60a78953391d90bb2b53b0b67717059f5328e4b0
Aliaksei_Ashukha <aliaksei_ashukha@epam.com>
2023-05-29 01:36:21 +0300

    Version proxy 1.3.3, backend 3.3.3

--- shell-installation/web/download_services.sh
+++ shell-installation/web/download_services.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Versions of the services
-HLM_BACKEND_VERSION="3.3.2"
-HLM_PROXY_VERSION="1.3.2"
+HLM_BACKEND_VERSION="3.3.3"
+HLM_PROXY_VERSION="1.3.3"
 
 # Downloading
 curl -L https://github.com/healenium/healenium-backend/releases/download/${HLM_BACKEND_VERSION}/healenium-backend-${HLM_BACKEND_VERSION}.jar > hlm-backend.jar
