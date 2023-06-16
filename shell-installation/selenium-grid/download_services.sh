42ab1d54dd67b8e1c25542ccaf13b9ca41216ca8
Aliaksei_Ashukha <aliaksei_ashukha@epam.com>
2023-06-16 17:13:41 +0300

    Version proxy 1.3.4

--- shell-installation/selenium-grid/download_services.sh
+++ shell-installation/selenium-grid/download_services.sh
@@ -2,7 +2,7 @@
 
 # Versions of the services
 HLM_BACKEND_VERSION="3.3.4"
-HLM_PROXY_VERSION="1.3.3"
+HLM_PROXY_VERSION="1.3.4"
 
 # Downloading
 curl -L https://github.com/healenium/healenium-backend/releases/download/${HLM_BACKEND_VERSION}/healenium-backend-${HLM_BACKEND_VERSION}.jar > hlm-backend.jar
