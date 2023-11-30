9def7dfc92fff45f0191ef6b332560ec9a7d629b
Aliaksei_Ashukha <aliaksei_ashukha@epam.com>
2023-11-30 16:09:15 +0300

    Update to 1.3.8

--- shell-installation/selenium-grid/download_services.sh
+++ shell-installation/selenium-grid/download_services.sh
@@ -2,7 +2,7 @@
 
 # Versions of the services
 HLM_BACKEND_VERSION="3.4.2"
-HLM_PROXY_VERSION="1.3.7"
+HLM_PROXY_VERSION="1.3.8"
 
 # Downloading
 curl -L https://github.com/healenium/healenium-backend/releases/download/${HLM_BACKEND_VERSION}/healenium-backend-${HLM_BACKEND_VERSION}.jar > hlm-backend.jar
